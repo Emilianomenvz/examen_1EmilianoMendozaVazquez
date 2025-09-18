ad7f30891dac7ad4d93cc15206cb50bb3c56bdaa
Emiliano Mendoza <194604-8@iberoleon.edu.mx>
2025-09-18 13:55:09 -0600

    corrección

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from clases import  areas  
 
 def main():
-
-    base_t, altura_t = 10, 5
-    print(f"Área del triángulo ({base_t}, {altura_t}):", areas.trian(base_t, altura_t))
-
-    base_r, altura_r = 8, 4
-    print(f"Área del rectángulo ({base_r}, {altura_r}):", areas.rectangulo(base_r, altura_r))
+   area.triangulo(3,4)
+   area.rectangulo(3,4)
 
 if __name__ == "__main__":
     main()
